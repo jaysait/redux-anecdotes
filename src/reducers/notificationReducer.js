220b545c44263e010c49ad16f9d6f57792157ad4
Jason Fleming <jason.fleming.se@gmail.com>
2021-11-13 04:28:01 -0700

    ex6.21 save timer in state

--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -1,8 +1,9 @@
-const notificationReducer = (state = '', action) => {
+const notificationReducer = (state = { timer: '', message: '' }, action) => {
   switch (action.type) {
     case 'SET_NOTIFICATION':
-      return action.message;
-
+      return { ...state, message: action.message };
+    case 'SET_TIMER':
+      return { ...state, timer: action.timer };
     default:
       return state;
   }
@@ -14,13 +15,26 @@ export const notificationChange = (message) => {
     message,
   };
 };
+const timerChange = (timer) => {
+  return {
+    type: 'SET_TIMER',
+    timer,
+  };
+};
 
 export const setNotification = (text, seconds) => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const st = getState();
+
+    if (st.message.timer) {
+      clearTimeout(st.message.timer);
+    }
     dispatch(notificationChange(text));
-    setTimeout(() => {
+    const timeoutId = setTimeout(() => {
       dispatch(notificationChange(''));
+      dispatch(timerChange(''));
     }, seconds * 1000);
+    dispatch(timerChange(timeoutId));
   };
 };
 
